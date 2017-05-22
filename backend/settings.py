3ce22fbe2db3886b7c6747dc0d638650d58b9791
brandoneprice31 <bprice@college.harvard.edu>
2017-05-22 14:31:24 -0400

    configuring for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'juhg9t0x$1e05mj)bdy#pv8famuqs@a7$m7s!i5%w-=2^4^^m!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['PYTHON_ENV'] != 'production'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brandonprice.org',
+                 '127.0.0.1']
 
 
 # Application definition
