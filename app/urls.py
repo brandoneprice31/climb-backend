31f80e2e513110f2e287744f0f332d0319f80ba6
brandoneprice31 <bprice@college.harvard.edu>
2017-05-29 21:13:35 -0700

    fixed

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from views import *
 
 urlpatterns = [
-    url(r'', index),
-    url(r'save_user_score', save_score),
-    url(r'users_scores', get_users_scores),
-    url(r'save_user_info', save_user_info)
+    url(r'^$', index),
+    url(r'^save_user_score$', save_score),
+    url(r'^users_scores$', get_users_scores),
+    url(r'^save_user_info$', save_user_info)
 ]
