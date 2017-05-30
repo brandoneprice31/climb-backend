7e0a423d341d7c42eebc5da5b2277e6874de420d
brandoneprice31 <bprice@college.harvard.edu>
2017-05-29 21:02:45 -0700

    prod db connected

--- app/lib/db.py
+++ app/lib/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 def db (collection):
-    client = MongoClient()
+    client = MongoClient('mongodb://brandonprice:#climbdatabaseyoyo@ds157571.mlab.com:57571/heroku_d3d5n4vx')
     return client['climb'][collection]
