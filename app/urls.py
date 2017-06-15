40022c2ddd3ef5a0d8ea352d9a7938d1e11b347f
brandoneprice31 <bprice@college.harvard.edu>
2017-06-14 20:50:22 -0700

    updated file structure

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,3 @@
-from django.conf.urls import url, include
-from views import *
+from .http_endpoints.api import *
 
-urlpatterns = [
-    url(r'^$', index),
-    url(r'^save_score$', save_score),
-    url(r'^get_users_scores$', get_users_scores),
-    url(r'^create_user$', create_user),
-    url(r'^get_user_info$', get_user_info),
-    url(r'^save_user_info$', save_user_info),
-    url(r'^get_friends_scores$', get_friends_scores),
-    url(r'^get_global_scores$', get_global_scores),
-    url(r'^get_rank$', get_rank),
-    url(r'^save_user_scores$', save_user_scores)
-]
+urlpatterns = url_method_map
